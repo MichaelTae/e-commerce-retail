182ff22637f0ce4db7668fc64fdc96d337eceadc
Michael S <Michael.Stojanovic@hotmail.com>
2023-07-21 15:37:44 +0200

    added sass + component paths

--- src/App.js
+++ src/App.js
@@ -1,31 +1,9 @@
+import { categories } from './data/static-home-data';
+import Directory from './components/directory/directory.component';
 const App = () => {
   return (
     <div className='categories-container'>
-      <div className='category-conainer'>{/* <img/> */}</div>
-      <div className='category-body-container'>
-        <h2>Hats</h2>
-        <p>Shop Now</p>
-      </div>
-      <div className='category-conainer'>{/* <img/> */}</div>
-      <div className='category-body-container'>
-        <h2>Jackets</h2>
-        <p>Shop Now</p>
-      </div>
-      <div className='category-conainer'>{/* <img/> */}</div>
-      <div className='category-body-container'>
-        <h2>Sneakers</h2>
-        <p>Shop Now</p>
-      </div>
-      <div className='category-conainer'>{/* <img/> */}</div>
-      <div className='category-body-container'>
-        <h2>Womens</h2>
-        <p>Shop Now</p>
-      </div>
-      <div className='category-conainer'>{/* <img/> */}</div>
-      <div className='category-body-container'>
-        <h2>Mens</h2>
-        <p>Shop Now</p>
-      </div>
+      <Directory categories={categories} />
     </div>
   );
 };

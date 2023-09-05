3549b6db06ae818d288f6513453df1fdfd913edb
Michael S <Michael.Stojanovic@hotmail.com>
2023-09-05 14:55:53 +0200

    added checkout navigation

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -3,8 +3,13 @@ import CartItem from '../cart-item/cart-item.component';
 import './cart-dropdown.styles.scss';
 import { useContext } from 'react';
 import { CartContext } from '../../context/cart.context';
+import { useNavigate } from 'react-router-dom';
 const CartDropdown = () => {
   const { cartItems } = useContext(CartContext);
+  const navigate = useNavigate();
+  const goToCheckoutHandler = () => {
+    navigate('/checkout');
+  };
   return (
     <div className='cart-dropdown-container'>
       <div className='cart-items'>
@@ -12,7 +17,8 @@ const CartDropdown = () => {
           <CartItem key={item.id} cartItem={item} />
         ))}
       </div>
-      <Button>GO TO CHECKOUT</Button>
+
+      <Button onClick={goToCheckoutHandler}>GO TO CHECKOUT</Button>
     </div>
   );
 };

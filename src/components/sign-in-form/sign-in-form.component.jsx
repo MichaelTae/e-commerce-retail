96c2fab556722ebc6894fb87086cdb6fa93e3f34
Michael S <Michael.Stojanovic@hotmail.com>
2023-09-25 13:05:09 +0200

    changed sign in to styled components

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -4,7 +4,7 @@ import {
   signInAuthUserWithEmailAndPassword,
 } from '../../utils/firebase/firebase.utils';
 import FormInput from '../form-input/form-input.component';
-import './sign-in-form.styles.scss';
+import { SignUpContainer } from './sign-in-form.styles';
 import Button, { BUTTON_TYPE_CLASSES } from '../button/button.component';
 const defaultFormFields = {
   email: '',
@@ -45,7 +45,7 @@ const SignInForm = () => {
   };
 
   return (
-    <div className='sign-up-container'>
+    <SignUpContainer>
       <h2>Already have an account?</h2>
       <span>Sign in with your email and password</span>
       <form onSubmit={handleSubmit}>
@@ -77,7 +77,7 @@ const SignInForm = () => {
           </Button>
         </div>
       </form>
-    </div>
+    </SignUpContainer>
   );
 };
 

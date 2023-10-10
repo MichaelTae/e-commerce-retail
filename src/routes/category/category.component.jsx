29c491e11c9d93fc26364622a62ccac07434cb23
Michael S <Michael.Stojanovic@hotmail.com>
2023-10-10 15:22:51 +0200

    added category reducer logic to components

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,11 +1,12 @@
 import { useParams } from 'react-router-dom';
-import { useContext, useState, useEffect } from 'react';
-import { CategoriesContext } from '../../context/categories.context';
+import { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { selectCategoriesMap } from '../../store/categories/categories.selector';
 import ProductCard from '../../components/product-card/product-card.component';
 import { CategoryContainer, CategoryTitle } from './category.styles.jsx';
 const Category = () => {
   const { category } = useParams();
-  const { categoriesMap } = useContext(CategoriesContext);
+  const categoriesMap = useSelector(selectCategoriesMap);
   const [products, setProducts] = useState(categoriesMap[category]);
   useEffect(() => {
     setProducts(categoriesMap[category]);

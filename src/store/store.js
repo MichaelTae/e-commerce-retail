3530ac69061bbafa95a7ccb68ab55aefed328e70
Michael S <Michael.Stojanovic@hotmail.com>
2023-10-12 12:54:56 +0200

    added logger filter + redux dev tools

--- src/store/store.js
+++ src/store/store.js
@@ -12,9 +12,16 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const middlewares = [logger];
+const middlewares = [process.env.NODE_ENV !== 'production' && logger].filter(
+  Boolean
+);
 
-const composedEnhancers = compose(applyMiddleware(...middlewares));
+const composeEnhancers =
+  (process.env.NODE_ENV !== 'production' &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
+
+const composedEnhancers = composeEnhancers(applyMiddleware(...middlewares));
 
 export const store = createStore(
   persistedReducer,
